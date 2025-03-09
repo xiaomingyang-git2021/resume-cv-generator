26b9de43e7b6f1e90ed2ffbe4fc0f7c76cf1aa82
xiaomingyang-git2021 <xiaomingyang@pursuit.org>
2025-03-08 22:40:15 -0500

    Update docs folder with new build files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Resume/CV Generator</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
