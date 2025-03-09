7b445249a1c7bcfb12cd5da2803f5d93b15a6502
xiaomingyang-git2021 <xiaomingyang@pursuit.org>
2025-03-09 11:10:53 -0400

    Update GitHub Pages deployment files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Resume/CV Generator</h1>
-        <p>
+        {/*<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
